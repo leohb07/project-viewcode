c43dd2007fce296c721c242ed45cdcef847046ff
leohb07 <leohb1501@gmail.com>
2022-04-30 10:24:17 -0300

    Criando projeto e limpando referencias do main.storyboard

---
+++ Project Viewcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project Viewcode
+//
+//  Created by Leonardo Henrique Barrocal on 30/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
